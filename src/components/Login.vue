6f60d3e300d002a3c36c1c32bb8bc62b27c4e5dc
spacecreatedd <kimzhenya1313@mail.ru>
2024-01-25 16:02:20 +0700

    login component

---
+++ src/components/Login.vue
@@ -0,0 +1,53 @@
+<template>
+    <form class="login" @submit.prevent="login">
+        <h1>Sign in</h1>
+        <label>User name</label>
+        <input type="text" required v-model="username" />
+        <label>Password</label>
+        <input type="password" required v-model="password" />
+        <hr />
+        <button type="submit">Login</button>
+    </form>
+</template>
+
+
+<style scoped>
+    .login{
+        display:flex;
+        flex-direction:column;
+        width:300px;
+        padding:10px;
+        margin:0 auto;
+    }
+    .login input, button{
+        border: 1px solid black;
+        border-radius:5px;
+    }
+    hr{
+        margin:10px 0;
+    }
+</style>
+
+
+<script>
+    export default{
+        data() {
+            return{
+                username:"",
+                password:"",
+            };
+        },
+        methods: {
+            login() {
+                const userData = {
+                    username: this.username,
+                    password: this.password,
+                };
+                
+                this.$store
+                    .dispatch(AUTH_REQUEST, userData)
+                    .then(() => this.$router.push("/"));
+            },
+        },
+    };
+</script>
--- src/store/index.js
+++ src/store/index.js
@@ -2,12 +2,36 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    token: localStorage.getItem('myAppToken') || '',
   },
   getters: {
+    isAuthenticated: (state) => !!state.token,
   },
   mutations: {
+    AUTH_SUCCESS: (state, token) => {
+      state.token = token;
+    },
+    AUTH_ERROR: (state) => {
+      state.token = '';
+    },
   },
   actions: {
+    AUTH_REQUEST: ({ commit }, user) => {
+      return new Promise((resolve, reject) => {
+        loginRequest(user)
+          .then((token) => {
+            commit('AUTH_SUCCESS', token);
+            localStorage.setItem('myAppToken', token);
+            resolve();
+          })
+          .catch(() => {
+            commit('AUTH_ERROR');
+            localStorage.removeItem('myAppToken');
+            reject();
+          });
+      });
+    },
+
   },
   modules: {
   }
